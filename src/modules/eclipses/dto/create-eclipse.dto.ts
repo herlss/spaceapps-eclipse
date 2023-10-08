06fa254ee0723b779e33eedf0bc3ced4912a98b5
Miguel Eros Romão Soares <miguelsoares-ieg@instituto.ad>
2023-10-08 12:34:01 -0300

    fix

--- src/modules/eclipses/dto/create-eclipse.dto.ts
+++ src/modules/eclipses/dto/create-eclipse.dto.ts
@@ -1,13 +1,29 @@
-import { ArrayNotEmpty, IsArray, IsDefined, IsString } from "class-validator";
+import { ArrayNotEmpty, IsArray, IsDefined, IsString, ValidateNested } from "class-validator";
 import { IAffected, IEclipse } from "../interfaces";
+import { Type } from "class-transformer";
+
+class AffectedDto implements IAffected {
+    @IsDefined()
+    @IsArray()
+    @ArrayNotEmpty()
+    @ValidateNested({ each: true })
+    @Type(() => Number)
+    coordinates: number[];
+
+    @IsDefined()
+    @IsString()
+    name: string;
+}
 
 export class CreateEclipseDto implements IEclipse {
     @IsDefined()
     @IsString()
     name: string;
-
+    
     @IsDefined()
     @IsArray()
     @ArrayNotEmpty()
-    affected: IAffected[];
+    @ValidateNested({ each: true })
+    @Type(() => AffectedDto)
+    affected: AffectedDto[];
 }
