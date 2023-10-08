35d47aefe8484e08e4b4109a095c58aafbe25df3
Miguel Eros Romão Soares <miguelsoares-ieg@instituto.ad>
2023-10-08 10:44:07 -0300

    fix

--- src/modules/users/users.gateway.ts
+++ src/modules/users/users.gateway.ts
@@ -1,89 +1,88 @@
-import { Injectable, UseInterceptors } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 import {
-  ConnectedSocket,
-  MessageBody,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-} from '@nestjs/websockets';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Server, Socket } from 'socket.io';
-import { User } from './schemas/user.schema';
-import { UsersInterceptor } from '../../common/interceptors/users.interceptor';
+    ConnectedSocket,
+    MessageBody,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from "@nestjs/websockets";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Server, Socket } from "socket.io";
+import { User } from "./schemas/user.schema";
 
 @Injectable()
 @WebSocketGateway({
-  namespace: '/game',
-  cors: { origin: process.env.APP_URL, methods: ['GET', 'POST'] },
+    namespace: "/game",
+    cors: { origin: process.env.APP_URL, methods: ["GET", "POST"] },
 })
 export class UsersGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(
+    constructor(
     @InjectModel(User.name)
     private readonly usersModel: Model<User>,
-  ) {}
+    ) {}
 
   @WebSocketServer()
   server: Server;
 
   async handleConnection(
     @ConnectedSocket()
-    client: Socket,
+        client: Socket,
   ) {
-    const room = client.handshake.query.room;
-    const char = client.handshake.query.character;
+      const room = client.handshake.query.room;
+      const char = client.handshake.query.character;
 
-    const user: User = await this.usersModel.create({
-      _id: client.id,
-      position: [0, 0],
+      const user: User = await this.usersModel.create({
+          _id: client.id,
+          position: [0, 0],
 	    room: room,
-      character: char
-    });
-    const users: User[] = await this.usersModel.find({ room: room });
+          character: char
+      });
+      const users: User[] = await this.usersModel.find({ room: room });
 
-    client.join(room);
+      client.join(room);
 
-    this.server.to(room).emit('users:joined', users);
-    client.emit('users:created', user);
+      this.server.to(room).emit("users:joined", users);
+      client.emit("users:created", user);
   }
 
   async handleDisconnect(
     @ConnectedSocket()
-    client: Socket,
+        client: Socket,
   ) {
-    const room: string =
+      const room: string =
       client.handshake.query.room instanceof Array
-        ? client.handshake.query.room[0]
-        : client.handshake.query.room;
+          ? client.handshake.query.room[0]
+          : client.handshake.query.room;
 
-    await this.usersModel.findByIdAndDelete(client.id);
+      await this.usersModel.findByIdAndDelete(client.id);
 
-    const users = await this.usersModel.find({ room: room });
+      const users = await this.usersModel.find({ room: room });
 
-    client.leave(room);
+      client.leave(room);
 
-    this.server.to(room).emit('users:updated', users);
+      this.server.to(room).emit("users:updated", users);
   }
 
-  @SubscribeMessage('users:updated')
+  @SubscribeMessage("users:updated")
   async handleUsersUpdate(
     @ConnectedSocket()
-    client: Socket,
+        client: Socket,
     @MessageBody()
-    user: User,
+        user: User,
   ) {
-    const room = client.handshake.query.room;
+      const room = client.handshake.query.room;
 
-    await this.usersModel.findByIdAndUpdate(client.id, {
-      $set: { position: user.position },
-    });
+      await this.usersModel.findByIdAndUpdate(client.id, {
+          $set: { position: user.position },
+      });
 
-    console.log(user);
+      console.log(user);
 
-    const users = await this.usersModel.find({ room: room });
+      const users = await this.usersModel.find({ room: room });
 
-    this.server.to(room).emit('users:updated', users);
+      this.server.to(room).emit("users:updated", users);
   }
 }
